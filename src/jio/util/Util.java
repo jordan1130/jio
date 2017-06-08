1f8fbf32e099aeddfd761e0ea579152e662dabfe
Janardan Singh <Janardan Singh@JanardanSingh>
2017-06-08 02:55:33 -0400

    update

--- src/jio/util/Util.java
+++ src/jio/util/Util.java
@@ -1,5 +1,21 @@
 package jio.util;
 
-public class Util {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.DataProvider;
 
+public class Util {
+	static WebDriver driver = null;
+
+	@DataProvider(name = "browser")
+	public void getBrowser(String browser) {
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./resourse/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+
+	}
 }
